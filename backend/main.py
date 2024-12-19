5f7f3cf4165d3e7fbe7d59cf8a82d35ff43d7a5c
LaRenzo <dlarenzo@outlook.com>
2024-12-18 23:59:12 -0700

    continue setting up file

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,17 @@
 
+#  IMPORT REQUEST AND JSONIFY(HELPS US TO RETURN JSON RESPONSES)
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+# RUN FLASK APP
+if __name__ == "__main__":
+    # CREATE DATABASE TABLES
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
 
 # CREATE
 # - We need first_name
