96bb60e0e09ceda0cd4ed9ce2aaf005c37666d82
LaRenzo <dlarenzo@outlook.com>
2024-12-19 11:23:16 -0700

    setup completed

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,25 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
+
+@app.route("/contacts", methods=["GET"])
+
 # RUN FLASK APP
 if __name__ == "__main__":
-    # CREATE DATABASE TABLES
+    # CREATE DATABASE TABLE IF IT DOESN'T EXIST
     with app.app_context():
         db.create_all()
     app.run(debug=True)
 
-
+#GET METHOD
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    # QUERY DATABASE FOR ALL CONTACTS
+    contacts = Contact.query.all()
+    # CONVERT ALL CONTACTS TO JSON
+    json_contacts = map(lambda x: x.to_json(), contacts)
+    # RETURN JSON RESPONSE
+    return jsonify(contacts_json)
 
 # CREATE
 # - We need first_name
